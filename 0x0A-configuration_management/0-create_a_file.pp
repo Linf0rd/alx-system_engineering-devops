a2f5ebb573d4b2503fa62fbedc85a3b336b9f7df
Linf0rd <linfordsnagger@gmx.com>
2023-12-01 22:24:27 +0200

    Create /tmp/school file with specified content and permissions
    Added a Puppet manifest file that creates a file named '/tmp/school' with the specified content 'I love Puppet', ownership of 'www-data', group ownership of 'www-data', and permissions set to '0744'.

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Creates a file named 'school' in the '/tmp' directory
 file { '/tmp/school':
-  ensure => present,
-  owner => 'www-data',
-  group => 'www-data',
-  mode => '0744',
+  ensure  => present,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
   content => 'I love Puppet',
 }
