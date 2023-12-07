b48435b9458ebfdc8ffa707e3f46efde263449e4
Linf0rd <linfordsnagger@gmx.com>
2023-12-07 16:28:47 +0200

    Install Flask version 2.1.0 using pip3
    Added Puppet code to install Flask version 2.1.0 using the pip3 provider.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,9 @@
-# Installs flask
+# Installs flask version 2.1.0 using pip3
 
+# Ensure the package is installed
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3'],
 }
+
+# End of Puppet manifest
