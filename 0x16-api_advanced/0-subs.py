87e41e35003c886150d6aefa9e8d267b1f36564d
Linf0rd <linfordsnagger@gmx.com>
2024-02-05 19:19:50 +0200

    Uncover Reddit's communities with a reliable function to fetch subscriber counts
    Key features:
    - Introduces the number_of_subscribers() function, a trusty tool for gauging subreddit engagement.
    - Seamlessly interacts with the Reddit API using requests for efficient data retrieval.
    - Constructs well-formatted API URLs for accurate subreddit targeting.
    - Includes a descriptive User-Agent header for responsible API interaction.
    - Handles potential redirects responsibly to ensure data integrity.
    - Parses JSON responses effectively to extract subscriber counts, providing valuable insights into community size.
    - Manages potential errors gracefully, returning 0 for invalid subreddits or unexpected issues, ensuring resilience.
    - Potential enhancements:
        - Explore API rate limiting and caching strategies for optimal performance and adherence to guidelines.
        - Implement more informative error handling to distinguish between specific issues (e.g., network errors, invalid subreddits, API errors).
        - Offer alternative output formats (e.g., CSV, JSON) for integration with other tools and analyses.
        - Consider storing retrieved data for historical tracking and trend analysis.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -19,22 +19,12 @@ def number_of_subscribers(subreddit):
         int: The number of subscribers or 0 if the subreddit is invalid.
     """
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'Chrome/121.0.0.0'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json().get('data')
-            if data:
-                return data.get('subscribers', 0)
-        return 0
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     except Exception as e:
         return 0
-
-
-if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
