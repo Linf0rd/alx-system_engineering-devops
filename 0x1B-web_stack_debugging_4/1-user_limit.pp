cdc57a843eaf301945a13ef6686eafc482d69b5e
Linf0rd <linfordsnagger@gmx.com>
2024-02-06 03:18:02 +0200

    Increase hard and soft file limits for holberton user
    Changes the hard and soft file limits for the user 'holberton' in /etc/security/limits.conf to 50000, addressing potential login and file access issues.

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# Allows the user holberton to login and access files without fail.
+
+exec { 'increase-hard-file-limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+	path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'increase-soft-file-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+	path    => '/usr/local/bin/:/bin/'
+}
